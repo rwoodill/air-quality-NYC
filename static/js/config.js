10d70abb7acb5402ad4b250cef46595c033eabc4
Rachel <40248698+rwoodill@users.noreply.github.com>
2023-12-03 09:55:49 -0500

    Delete static/js/config.js

--- static/js/config.js
+++
@@ -1,7 +0,0 @@
-const EMAIL = "rachelwoodill0526@gmail.com";
-
-const API_KEY = "goldcat18";
-
-const CONNECTION_STRING = "mongodb+srv://public:eodWv15UpDSHXqUZ@cluster0.wjypgq6.mongodb.net/";
-
-const DB_NAME = "project_three_data";

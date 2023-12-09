b905483c89b826891ea2ae58f707a845be14bd58
ZHAO W <54wenzhao@gmail.com>
2023-12-08 21:17:49 -0500

    chart js finished

--- static/js/logic_chart.js
+++ static/js/logic_chart.js
@@ -1,41 +1,64 @@
 //const { config } = require("module");
 
-//set chart element
-const ctx = document.getElementById('myChart');
+function generateChartdata(chartid){
+    var chart_dataset = []
+    var line_label = []
+    var chart_labels = []
+    var url1 = `/api/get_chart_data/chart?geoid=1&chartid=${chartid}`
+    d3.json(url1).then((data) => {
+        for (let i = 1; i < Object.keys(data).length; i++){
+            chart_labels.push(data[i].Time)
+        }});
 
-var chart_labels = []
-var chart_data = []
-
-let config = {
-    data: {
-        datasets: [{
-            type: 'bar',
-            label: 'Bar Dataset',
-            data: [10, 20, 30, 40]
-        }, {
+    for (let i =1; i<6; i++){
+        var geoid = i 
+        var data_temp = []
+        var label_temp = []
+        var url = `/api/get_chart_data/chart?geoid=${geoid}&chartid=${chartid}`
+        d3.json(url).then((data) => {
+            for (let i = 1; i < Object.keys(data).length; i++){
+                data_temp.push(data[i].Value)
+                line_label = data[i].Geography
+            }
+        var dataset = {
             type: 'line',
-            label: 'Line Dataset',
-            data: [50, 50, 50, 50],
-        }],
-        labels: ['January', 'February', 'March', 'April']
-    },
+            label: line_label,
+            data: data_temp
+        }
+        chart_dataset.push(dataset)
+        data_temp = []
+        });
+    };
+
+    let config = {
+        data: {
+            datasets: chart_dataset,
+            labels: chart_labels
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: chartid
+                }
+            }
+        }
+    }
+    return config
+}
+//set chart element
+for (let j =1; j<4; j++){
+    if (j === 1){
+        chartid = 'asthma'
+    } else if (j === 2){
+        chartid = 'fineparticles'
+    } else if (j === 3){
+        chartid = 'nox'
+    }
+
+    const ctx = document.getElementById(`myChart${j}`);
+    var config = generateChartdata(chartid)
+    const asthmaChart = new Chart(ctx, config);
 }
 
-for (let i =1; i<2; i++){
-    var geoid = i 
-    chartid = 'asthma'
-    var url = `/api/get_chart_data/chart?geoid=${geoid}&chartid=${chartid}`
-    d3.json(url).then((data) => {
-        for (let i =1; i < Object.keys(data).length; i++){
-            chart_labels.push(data[i].Time)
-            chart_data.push(data[i].Value)
-        }
-    });
-    console.log(chart_config)
-};
-
-
-chart_config.push(config)
-
-const mixedChart = new Chart(ctx, config);
 

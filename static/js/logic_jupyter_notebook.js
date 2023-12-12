07beb409c1a63f0762c81673f2d4dae2ccae1e1d
rwoodill <woodillrachel@gmail.com>
2023-12-12 11:12:32 -0500

    removing unnecessary code

--- static/js/logic_jupyter_notebook.js
+++
@@ -1,8 +0,0 @@
-//-------------------------------------------------------------------------------------------------
-// function to render the jupyter notebook on dropdown select
-//-------------------------------------------------------------------------------------------------
-
-function renderJupyterNotebook(){
-    d3.select("#example-content").appendHTML("data_analysis.html");
-}
-renderJupyterNotebook();
